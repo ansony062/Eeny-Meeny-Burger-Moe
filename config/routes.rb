49bf5708832ef354777e564bf70bd8e44625a794
ansony062 <141472045+ansony062@users.noreply.github.com>
2023-10-10 09:05:56 +0000

    gem導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  
+  #管理者側
+  devise_for :admin, skip: [:registrations, :passwords], controllers:{
+    sessions: "admin/sessions"
+  }
+  #顧客側
+  devise_for :users, skip: [:password], controllers:{
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  #顧客
+  #管理者
 end
