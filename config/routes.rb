42fc1817a321b6a02d3d1876a977cbc9a559b978
ansony062 <141472045+ansony062@users.noreply.github.com>
2023-10-10 16:02:00 +0000

    routes追加

--- config/routes.rb
+++ config/routes.rb
@@ -14,11 +14,28 @@ Rails.application.routes.draw do
   #管理者
   namespace :admin do
     get '/' => 'posts#index'
+    resources :users, only: [:index, :show, :edit, :update]
+    resources :posts, only: [:index, :show, :edit, :update]
+    resources :comments, only: [:show, :destroy]
+    resources :tags, only: [:index, :new, :edit, :update]
   end
 
   #顧客
   scope module: :public do
     root to: 'homes#top'
+    get 'users/confirm' => 'users#confirm'
+    resources :users, only: [:show, :edit, :update] do
+      resource :relationships, only: [:create, :destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'reltionships#followers', as: 'followers'
+    end
+    resources :posts, only: [:index, :show, :new, :edit, :update] do
+      resource :favorites, only: [:create, :destroy]
+      resources :comments, only: [:create, :destroy]
+      resources :bookmarks, only: [:index, :create, :destroy]
+    end
+    resources :tags, only: [:index]
+    get 'users/seach', to: 'searches#search'
   end
 
 end
