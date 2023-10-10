9b006e6612ce24d676a5e77f7f0f18c2dcec97b6
ansony062 <141472045+ansony062@users.noreply.github.com>
2023-10-10 16:55:12 +0000

    アソシエーション追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+
+  has_many :bookmarks, dependent: :destroy
+  has_many :comments,  dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+  has_many :post_tags, dependent: :destroy
+  has_many :tags, through: :post_tags
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :bookmarks, dependent: :destroy
+  has_many :comments,  dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
 end
