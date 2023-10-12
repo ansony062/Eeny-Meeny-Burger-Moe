f385035e59de580b96835e660724865cf48ebde7
ansony062 <141472045+ansony062@users.noreply.github.com>
2023-10-12 19:44:04 +0000

    退会機能

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,2 +1,10 @@
 class Public::UsersController < ApplicationController
+  
+  def withdrawal
+    @user = User.find(current_user.id)
+    @user.update(is_active: false) #is_activeをfalse(退会)に変更
+    reset_session
+    flash[:notice] = "退会しました"
+    redirect_to root_path
+  end
 end
