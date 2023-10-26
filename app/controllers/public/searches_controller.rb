f663046c4a4c6e77c533f3e559d47792021557f0
ansony062 <141472045+ansony062@users.noreply.github.com>
2023-10-26 12:25:00 +0000

    検索機能

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,6 +1,7 @@
 class Public::SearchesController < ApplicationController
 
   def search
+    #byebug
     @keyword = params[:keyword]
     @model = params[:model]
 
@@ -9,7 +10,12 @@ class Public::SearchesController < ApplicationController
     elsif @model == "post"
       @records = Post.search_for(@keyword)
     elsif @model == "tag"
-      @records = Tag.search_for(@keyword)
+      @tag = Tag.search_for(@keyword)
+      @om = (@tag.to_a)[0].id
+      @post_tag = PostTag.where(tag_id: @om)
+      @mapost = @post_tag.to_a.map(&:post_id)
+      @post = Post.where(id: @mapost)
+      @records = @post.to_a
     end
   end
 
