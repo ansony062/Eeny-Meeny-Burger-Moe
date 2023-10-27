58dbb90f026e1b57cb1a087e894a9240c8100c40
ansony062 <141472045+ansony062@users.noreply.github.com>
2023-10-27 13:28:19 +0000

    検索コントローラーにコメント

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -11,11 +11,11 @@ class Public::SearchesController < ApplicationController
       @records = Post.search_for(@keyword)
     elsif @model == "tag"
       @tag = Tag.search_for(@keyword)
-      @om = (@tag.to_a)[0].id
-      @post_tag = PostTag.where(tag_id: @om)
-      @mapost = @post_tag.to_a.map(&:post_id)
-      @post = Post.where(id: @mapost)
-      @records = @post.to_a
+      @om = (@tag.to_a)[0].id                   #キーワードのあるタグのidを取得
+      @post_tag = PostTag.where(tag_id: @om)    #post_tagからすべてのキーワードのあるタグのidを取得
+      @mapost = @post_tag.to_a.map(&:post_id)   #タグidのついた投稿のidを取得
+      @post = Post.where(id: @mapost)           #タグのついた投稿のidをすべて取得
+      @records = @post.to_a                     #投稿を取得
     end
   end
 
