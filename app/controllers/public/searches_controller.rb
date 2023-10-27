eada02ab36c5f7102528d69572bcaaa0b819c99d
ansony062 <141472045+ansony062@users.noreply.github.com>
2023-10-27 14:28:31 +0000

    タグ検索のタグがない時追加

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,7 +1,6 @@
 class Public::SearchesController < ApplicationController
 
   def search
-    #byebug
     @keyword = params[:keyword]
     @model = params[:model]
 
@@ -11,11 +10,15 @@ class Public::SearchesController < ApplicationController
       @records = Post.search_for(@keyword)
     elsif @model == "tag"
       @tag = Tag.search_for(@keyword)
-      @om = (@tag.to_a)[0].id                   #キーワードのあるタグのidを取得
-      @post_tag = PostTag.where(tag_id: @om)    #post_tagからすべてのキーワードのあるタグのidを取得
-      @mapost = @post_tag.to_a.map(&:post_id)   #タグidのついた投稿のidを取得
-      @post = Post.where(id: @mapost)           #タグのついた投稿のidをすべて取得
-      @records = @post.to_a                     #投稿を取得
+      if @tag.present?
+        @om = (@tag.to_a)[0].id                   #キーワードのあるタグのidを取得
+        @post_tag = PostTag.where(tag_id: @om)    #post_tagからすべてのキーワードのあるタグのidを取得
+        @mapost = @post_tag.to_a.map(&:post_id)   #タグidのついた投稿のidを取得
+        @post = Post.where(id: @mapost)           #タグのついた投稿のidをすべて取得
+        @records = @post.to_a                     #投稿を取得
+      else
+      @redords = []                               #@tagが存在しない場合
+      end
     end
   end
 
